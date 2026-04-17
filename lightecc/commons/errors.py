02ef6309ce62f5c3735e9cdaffa96accaf1e21ee
Sefik Ilkin Serengil <serengil@gmail.com>
2026-04-17 08:26:22 +0100

    point not on curve and invalid curve order error types added

---
+++ lightecc/commons/errors.py
@@ -0,0 +1,6 @@
+class InvalidCurveOrder(ValueError):
+    """Raised when the order of the curve is invalid."""
+
+
+class PointNotOnCurve(ValueError):
+    """Raised when a point is not on the curve."""
